f3073bdd3c0ed1eeb4836dea62f905ed71ebfc42
Naitik Shah <n@daaku.org>
2010-05-09 00:16:30 +0000

    include JS enabled login button and background status ping in example

--- examples/example.php
+++ examples/example.php
@@ -41,7 +41,7 @@ $naitik = $facebook->api('/naitik');
 
 ?>
 <!doctype html>
-<html>
+<html xmlns:fb="http://www.facebook.com/2008/fbml">
   <head>
     <title>php-sdk</title>
     <style>
@@ -58,6 +58,35 @@ $naitik = $facebook->api('/naitik');
     </style>
   </head>
   <body>
+    <!--
+      We use the JS SDK to provide a richer user experience. For more info,
+      look here: http://github.com/facebook/connect-js
+    -->
+    <div id="fb-root"></div>
+    <script>
+      window.fbAsyncInit = function() {
+        FB.init({
+          appId  : '<?php echo $facebook->getAppId(); ?>',
+          status : true, // check login status
+          cookie : true, // enable cookies to allow the server to access the session
+          xfbml  : true  // parse XFBML
+        });
+
+        // whenever the user logs in, we refresh the page
+        FB.Event.subscribe('auth.login', function() {
+          window.location.reload();
+        });
+      };
+
+      (function() {
+        var e = document.createElement('script');
+        e.src = document.location.protocol + '//connect.facebook.net/en_US/all.js';
+        e.async = true;
+        document.getElementById('fb-root').appendChild(e);
+      }());
+    </script>
+
+
     <h1><a href="example.php">php-sdk</a></h1>
 
     <?php if ($me): ?>
@@ -65,9 +94,15 @@ $naitik = $facebook->api('/naitik');
       <img src="http://static.ak.fbcdn.net/rsrc.php/z2Y31/hash/cxrz4k7j.gif">
     </a>
     <?php else: ?>
-    <a href="<?php echo $loginUrl; ?>">
-      <img src="http://static.ak.fbcdn.net/rsrc.php/zB6N8/hash/4li2k73z.gif">
-    </a>
+    <div>
+      Using JavaScript &amp; XFBML: <fb:login-button></fb:login-button>
+    </div>
+    <div>
+      Without using JavaScript &amp; XFBML:
+      <a href="<?php echo $loginUrl; ?>">
+        <img src="http://static.ak.fbcdn.net/rsrc.php/zB6N8/hash/4li2k73z.gif">
+      </a>
+    </div>
     <?php endif ?>
 
     <h3>Session</h3>
