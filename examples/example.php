a1d484435fbf1b6717e7012d4e51f3cba88d9621
Paul Tarjan <paul@paulisageek.com>
2010-05-13 19:21:17 -0700

    group the variables together (with booleans after) and fix comma

--- examples/example.php
+++ examples/example.php
@@ -67,10 +67,10 @@ $naitik = $facebook->api('/naitik');
       window.fbAsyncInit = function() {
         FB.init({
           appId   : '<?php echo $facebook->getAppId(); ?>',
+          session : <?php echo json_encode($session); ?>, // don't refetch the session when PHP already has it
           status  : true, // check login status
           cookie  : true, // enable cookies to allow the server to access the session
-          xfbml   : true  // parse XFBML
-          session : <?php echo json_encode($session); ?>, // don't refetch the session when PHP already has it
+          xfbml   : true // parse XFBML
         });
 
         // whenever the user logs in, we refresh the page
