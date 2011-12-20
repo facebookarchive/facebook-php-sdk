2d723adb5a53ad02b494744d5e1060b34823d965
Paul Tarjan <spam@paulisageek.com>
2011-12-20 11:17:33 -0800

    Changed the App ID again since someone hijacked the last one. This is why we can't have nice things.
    https://github.com/facebook/php-sdk/commit/54acf92444c8acdbe89228629331a7f2e6e50f85

--- examples/example.php
+++ examples/example.php
@@ -19,8 +19,8 @@ require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => '191149314281714',
-  'secret' => '73b67bf1c825fa47efae70a46c18906b',
+  'appId'  => '344617158898614',
+  'secret' => '6dc8ac871858b34798bc2488200e503d',
 ));
 
 // Get User ID
