4ea94b975f9aee86e5d25c111c94081360a11171
Paul Tarjan <spam@paulisageek.com>
2011-12-20 16:18:16 -0800

    Same app id as https://github.com/facebook/php-sdk/commit/2d723adb5a53ad02b494744d5e1060b34823d965

--- examples/with_js_sdk.php
+++ examples/with_js_sdk.php
@@ -3,8 +3,8 @@
 require '../src/facebook.php';
 
 $facebook = new Facebook(array(
-  'appId'  => '191149314281714',
-  'secret' => '73b67bf1c825fa47efae70a46c18906b',
+  'appId'  => '344617158898614',
+  'secret' => '6dc8ac871858b34798bc2488200e503d',
 ));
 
 // See if there is a user from a cookie
