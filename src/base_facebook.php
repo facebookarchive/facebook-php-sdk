f336af0f6631743573a797c729d4e740e57001a3
Matt Kelly <matthwk@gmail.com>
2012-06-29 15:46:13 -0700

    Fixes spacing issues.

--- src/base_facebook.php
+++ src/base_facebook.php
@@ -330,41 +330,46 @@ abstract class BaseFacebook
   }
 
   /**
-   * Extend an access token, while removing the short-lived token that might have been generated via client-side flow.
-   * Thanks to http://stackoverflow.com/questions/486896/adding-a-parameter-to-the-url-with-javascript for the workaround
+   * Extend an access token, while removing the short-lived token that might
+   * have been generated via client-side flow. Thanks to http://bit.ly/b0Pt0H
+   * for the workaround.
    */
-  public function getExtendedAccessToken() {
+  public function setExtendedAccessToken() {
     try {
       // need to circumvent json_decode by calling _oauthRequest
       // directly, since response isn't JSON format.
-      $access_token_response =
-        $this->_oauthRequest(
-          $this->getUrl('graph', '/oauth/access_token'),
-          $params = array(    'client_id' => $this->getAppId(),
+      $access_token_response = $this->_oauthRequest(
+        $this->getUrl('graph', '/oauth/access_token'),
+        $params = array(
+          'client_id' => $this->getAppId(),
           'client_secret' => $this->getAppSecret(),
-          'grant_type'=>'fb_exchange_token',
-          'fb_exchange_token'=>$this->getAccessToken(),
-        ));
-      } catch (FacebookApiException $e) {
-        // most likely that user very recently revoked authorization.
-        // In any event, we don't have an access token, so say so.
-        return false;
-      }
+          'grant_type' => 'fb_exchange_token',
+          'fb_exchange_token' => $this->getAccessToken(),
+        )
+      );
+    }
+    catch (FacebookApiException $e) {
+      // most likely that user very recently revoked authorization.
+      // In any event, we don't have an access token, so say so.
+      return false;
+    }
   
-      if (empty($access_token_response)) {
-        return false;
-      }
+    if (empty($access_token_response)) {
+      return false;
+    }
       
-      $response_params = array();
-      parse_str($access_token_response, $response_params);
-      
-      if (!isset($response_params['access_token'])) {
-        return false;
-      }
-      
-      $this->destroySession();
-      
-      $this->setPersistentData('access_token', $response_params['access_token']);
+    $response_params = array();
+    parse_str($access_token_response, $response_params);
+    
+    if (!isset($response_params['access_token'])) {
+      return false;
+    }
+    
+    $this->destroySession();
+    
+    $this->setPersistentData(
+      'access_token', $response_params['access_token']
+    );
   }
 
   /**
