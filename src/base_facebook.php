9093fe48ed87bf13a17116e4ba4af67d23ecf094
Ankit <ankithbti007@gmail.com>
2012-12-05 20:49:16 +0530

    Reverting back the changes. Found another clean way to do it.

--- src/base_facebook.php
+++ src/base_facebook.php
@@ -596,7 +596,6 @@ abstract class BaseFacebook
    * @return string The URL for the logout flow
    */
   public function getLogoutUrl($params=array()) {
-    session_destroy();
     return $this->getUrl(
       'www',
       'logout.php',
