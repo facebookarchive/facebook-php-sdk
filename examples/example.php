5193c5b8ae5e1844801607061713112ebb8f5104
mohammadnasr <m00n_mail@hotmail.com>
2013-03-22 20:12:57 +0200

    Update example.php

--- examples/example.php
+++ examples/example.php
@@ -19,8 +19,8 @@ require '../src/facebook.php';
 
 // Create our Application instance (replace this with your appId and secret).
 $facebook = new Facebook(array(
-  'appId'  => '344617158898614',
-  'secret' => '6dc8ac871858b34798bc2488200e503d',
+  'appId'  => '493405234053277',
+  'secret' => '  337b105b33b51b2b73c0a613a55a3ecf',
 ));
 
 // Get User ID
