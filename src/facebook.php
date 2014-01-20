382092a67f8bf1f71c7bb9633da0fa6b7ba3bf8d
Yervand Khalapyan <ervandos@gmail.com>
2014-01-20 05:59:03 -0800

    Added expired date timestamp
    Added expired date timestamp in Supported keys for persistent data

--- src/facebook.php
+++ src/facebook.php
@@ -76,11 +76,11 @@ class Facebook extends BaseFacebook
 
   /**
    * Supported keys for persistent data
-   *
+   * Added expired date timestamp
    * @var array
    */
   protected static $kSupportedKeys =
-    array('state', 'code', 'access_token', 'user_id');
+    array('state', 'code', 'access_token', 'user_id', 'expires');
 
   /**
    * Initiates Shared Session
