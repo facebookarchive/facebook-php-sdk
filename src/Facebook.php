58d9d99d7eb762f3c3889dd3989512fc6f593503
Maxime Fabre <ehtnam6@gmail.com>
2014-02-10 17:11:35 +0100

    Namespace classes

--- src/Facebook.php
+++ src/Facebook.php
@@ -14,8 +14,7 @@
  * License for the specific language governing permissions and limitations
  * under the License.
  */
-
-require_once "base_facebook.php";
+namespace Facebook;
 
 /**
  * Extends the BaseFacebook class with the intent of using
